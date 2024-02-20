252e6e3937829237138e1f64521affd6475f17f3
Bruno Vinícius <brunovinicius082005@gmail.com>
2024-02-20 15:47:05 -0300

    feat: create a dto for user make login

---
+++ UserAPI/Data/Dtos/User/UserSignInDto.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UserAPI.Data.Dtos;
+
+public class UserSignInDto
+{
+    [Required] public string UserName { get; set; }
+    [Required] public string Password { get; set; }
+}
