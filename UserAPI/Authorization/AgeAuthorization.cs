087552771ce73c432a3cd242d5555e21263790f1
Bruno Vinícius <brunovinicius082005@gmail.com>
2024-02-20 15:50:40 -0300

    feat: create a bussines rule for user validade

---
+++ UserAPI/Authorization/AgeAuthorization.cs
@@ -0,0 +1,27 @@
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
+
+namespace UserAPI.Authorization;
+
+public class AgeAuthorization : AuthorizationHandler<MinimalAge>
+{
+    protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, MinimalAge requirement)
+    {
+        var birthDataClaim = context.User.FindFirst(claim => claim.Type == ClaimTypes.DateOfBirth);
+        if (birthDataClaim is null) return Task.CompletedTask;
+
+        var birthData = Convert.ToDateTime(birthDataClaim.Value);
+        var userAge = DateTime.Today.Year - birthData.Year;
+        if (birthData > DateTime.Today.AddYears(-userAge))
+        {
+            userAge--;
+        }
+
+        if (userAge >= requirement.Age)
+        { 
+            context.Succeed(requirement);
+        }
+
+        return Task.CompletedTask;
+    }
+}
