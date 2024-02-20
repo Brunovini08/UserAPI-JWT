7e27bb9bb795ad055ca65ef81897f910d0b961db
Bruno Vinícius <brunovinicius082005@gmail.com>
2024-02-20 15:49:17 -0300

    feat: create token service that will created the token

---
+++ UserAPI/Services/TokenService.cs
@@ -0,0 +1,28 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+using UserAPI.Models;
+
+namespace UserAPI.Services;
+
+public class TokenService
+{
+   public string GenerateToken(User user)
+   {
+      Claim[] claims = new[]
+      {
+         new Claim("id", user.Id),
+         new Claim("userName", user.UserName),
+         new Claim(ClaimTypes.DateOfBirth, user.BirthDate.ToString())
+      };
+      var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("OFISJDFOSIDFJ029348DFSKFJDSLKJF090FDSJ"));
+
+      var signingCredentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+      var token = new JwtSecurityToken(expires: DateTime.Now.AddMinutes(10), claims: claims,
+         signingCredentials: signingCredentials);
+
+      return new JwtSecurityTokenHandler().WriteToken(token);
+   }
+}
