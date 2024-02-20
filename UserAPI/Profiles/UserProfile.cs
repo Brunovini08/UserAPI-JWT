575d03a8cb7934f5615b11bd0512e8fcb0fc9de9
Bruno Vinícius <brunovinicius082005@gmail.com>
2024-02-20 15:51:48 -0300

    feat: create a mapper for user

---
+++ UserAPI/Profiles/UserProfile.cs
@@ -0,0 +1,12 @@
+﻿using UserAPI.Data.Dtos;
+using UserAPI.Models;
+
+namespace UserAPI.Profiles;
+
+public class UserProfile : AutoMapper.Profile
+{
+    public UserProfile()
+    {
+        CreateMap<CreateUserDto, User>();
+    }
+}
