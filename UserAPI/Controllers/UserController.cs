6e83b5c97d69ea570e8e009a131f8af3dd2dd441
Bruno Vinícius <brunovinicius082005@gmail.com>
2024-02-20 15:43:28 -0300

    feat: create a user controller

---
+++ UserAPI/Controllers/UserController.cs
@@ -0,0 +1,34 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using UserAPI.Data.Dtos;
+using UserAPI.Models;
+using UserAPI.Services;
+
+namespace UserAPI.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class UserController : ControllerBase
+{
+    private UserService _userService;
+
+    public UserController(UserService userService)
+    {
+        _userService = userService;
+    }
+    
+    [HttpPost("signup")]
+    public async Task<IActionResult> SignupUser([FromBody] CreateUserDto createUserDto)
+    {
+        await _userService.SignUp(createUserDto);
+        return Ok("User signUp");
+    }
+
+    [HttpPost("signin")]
+    public async Task<IActionResult> SignInUser([FromBody] UserSignInDto userSignInDto)
+    {
+        var token = await _userService.SignIn(userSignInDto);
+        return Ok(token);
+    }
+}
