aae37b0d3dd93fb2cd130617a33b93c8c27b4bc8
Bruno Vinícius <brunovinicius082005@gmail.com>
2024-02-20 15:50:16 -0300

    feat: create a bussines rule for validate a user

---
+++ UserAPI/Authorization/MinimalAge.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Authorization;
+
+namespace UserAPI.Authorization;
+
+public class MinimalAge : IAuthorizationRequirement
+{
+    public MinimalAge(int age)
+    {
+        Age = age;
+    }
+    public int Age { get; set; }
+}
