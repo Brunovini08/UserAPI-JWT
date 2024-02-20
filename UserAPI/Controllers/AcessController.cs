f45f76a2f115162a37077e0401abac9676d80ce5
Bruno Vinícius <brunovinicius082005@gmail.com>
2024-02-20 15:42:28 -0300

    feat: create a controller of acess

---
+++ UserAPI/Controllers/AcessController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UserAPI.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class AcessController : ControllerBase
+{
+    [HttpGet]
+    [Authorize(Policy = "MinimalAge")]
+    public IActionResult Get()
+    {
+        return Ok("Authorize acess");
+    }
+}
