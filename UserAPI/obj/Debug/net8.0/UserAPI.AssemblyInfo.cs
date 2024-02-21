02c9de0ee86d5f170c1130a4bc701f0d6b412d1f
Bruno Vinícius <brunovinicius082005@gmail.com>
2024-02-21 11:38:37 -0300

    System files

--- UserAPI/obj/Debug/net8.0/UserAPI.AssemblyInfo.cs
+++ UserAPI/obj/Debug/net8.0/UserAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UserAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+81d5a1955731ecca1d2fb1665d8ce4eb83af151c")]
 [assembly: System.Reflection.AssemblyProductAttribute("UserAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UserAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
